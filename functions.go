b4f3aafbb538ec9d04a23e05fe82c9b4f50ff67a
Peyton Walters <pawalt@hey.com>
2023-06-17 00:49:05 -0400

    transcripts/better_protections.txt

--- functions.go
+++ functions.go
@@ -268,16 +268,11 @@ func GlobFiles(raw string) map[string][]string {
 	_ = json.Unmarshal([]byte(raw), &req)
 	pattern := req.Pattern
 
-	// Ensure the pattern doesn't go outside the current directory
+	// Get the absolute path of the current directory
 	currDir, err := filepath.Abs(".")
 	if err != nil {
 		return map[string][]string{
 			"error": {"could not determine current directory"},
-		}
-	}
-	if !strings.HasPrefix(pattern, currDir) {
-		return map[string][]string{
-			"error": {"pattern attempts to escape current directory"},
 		}
 	}
 
@@ -286,8 +281,23 @@ func GlobFiles(raw string) map[string][]string {
 		return map[string][]string{}
 	}
 
+	var validMatches []string
+	for _, match := range matches {
+		absMatch, err := filepath.Abs(match)
+		if err != nil {
+			return map[string][]string{
+				"error": {fmt.Sprintf("could not determine absolute path of match: %v", err)},
+			}
+		}
+
+		// Check if the match is in the current directory or a subdirectory
+		if strings.HasPrefix(absMatch, currDir) {
+			validMatches = append(validMatches, match)
+		}
+	}
+
 	return map[string][]string{
-		"filepaths": matches,
+		"filepaths": validMatches,
 	}
 }
 
